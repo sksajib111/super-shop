1e93ec484575c74282940a2ead52d16b473e1dbc
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2023-11-19 23:32:53 +0600

    Order page customization and implements

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,9 +1,11 @@
-
+import orderCoImg from '../../../assets/order/order.jpg';
+import Cover from '../../../pages/Shared/Cover/Cover';
 
 const Order = () => {
   return (
     <div>
-      
+      <Cover img={orderCoImg} title={"Order Food"} description={"Would you like to try a dish?"}></Cover>
+      <h1>This is Order page</h1>
     </div>
   );
 };
