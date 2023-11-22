b1941b91737287c2e20f28718cc8c41d28769696
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2023-11-22 23:53:45 +0600

    Home page update and debugging some errror

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,15 +4,25 @@ import Category from "../Category/Category";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
 import Featured from "../featured/Featured";
+import Card from "../../Shared/Card/Card";
+import useMenu from "../../../hooks/useMenu";
 
 
 const Home = () => {
+  const [menu] = useMenu();
+  const salad = menu.filter(item => item.category === 'salad');
+
   return (
     <div>
       <Helmet><title>Super-Shop | Home</title></Helmet>
       <Banner></Banner>
       <Category></Category>
       <PopularMenu></PopularMenu>
+      <div className="grid md:grid-cols-3 gap-4">
+      {salad.map((item) => (
+        <Card key={item._id} item={item}></Card>
+      ))}
+    </div>
       <Featured></Featured>
       <Testimonials></Testimonials>
     </div>
