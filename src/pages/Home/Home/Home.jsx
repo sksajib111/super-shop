5c4e4d5a8f0900e123537e59706f031f385957e9
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2023-11-23 18:54:56 +0600

    Implements some features

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -6,17 +6,19 @@ import Testimonials from "../Testimonials/Testimonials";
 import Featured from "../featured/Featured";
 import Card from "../../Shared/Card/Card";
 import useMenu from "../../../hooks/useMenu";
-
+import Cover from "../../Shared/Cover/Cover";
+import coverImg from "../../../assets/menu/dessert-bg.jpeg";
 
 const Home = () => {
   const [menu] = useMenu();
-  const salad = menu.filter(item => item.category === 'salad');
+  const salad = menu.filter(item => item.category === 'drinks');
 
   return (
     <div>
       <Helmet><title>Super-Shop | Home</title></Helmet>
       <Banner></Banner>
       <Category></Category>
+      <Cover img={coverImg} title={"Super Shope"} description={"Warm goats cheese and roasted vegetable salad with black olive tapenade crostiniPan roasted pork belly with gratin potato, braised Savoy cabbage, apples, thyme and calvados jusRoasted duck breast (served pink) with gratin potato"}></Cover>
       <PopularMenu></PopularMenu>
       <div className="grid md:grid-cols-3 gap-4">
       {salad.map((item) => (
--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -5,7 +5,7 @@ const Cover = ({img, title, description}) => {
   return (
     <div className="hero h-[600px]" style={{backgroundImage: `url(${img})`}}>
   <div className="hero-overlay bg-opacity-50"></div>
-  <div className="hero-content text-center bg-slate-900 bg-opacity-40 rounded-3xl text-neutral-content">
+  <div className="hero-content text-center bg-slate-950 bg-opacity-40 rounded-3xl text-neutral-content">
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
       <p className="mb-5">{description}</p>
