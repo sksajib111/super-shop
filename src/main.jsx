93cc3f778570da81257d42886699dd855dd68db6
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2023-11-28 23:41:50 +0600

    Update some page

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { HelmetProvider } from "react-helmet-async";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <div className="max-w-screen-xl mx-auto">
+      <div className="max-w-screen-xl mx-auto bg-white">
         <RouterProvider router={router} />
       </div>
     </HelmetProvider>
