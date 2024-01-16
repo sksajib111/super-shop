11d07fac8716d00fc3c3573941ce356c7d3fb53d
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-01-16 22:15:50 +0600

    pagination apply in order tab items

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,11 +1,33 @@
 import Card from "../../Shared/Card/Card";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 const OrderTab = ({items}) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  };
   return (
     <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4">
-      {items.map((item) => (
+      
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        {items.map((item) => (
         <Card key={item._id} item={item}></Card>
       ))}
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
