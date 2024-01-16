419145d6d3ea29ba374f97700b4548823084a4b0
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-01-16 22:37:17 +0600

    create login page

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,31 +1,32 @@
 import Card from "../../Shared/Card/Card";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
-const OrderTab = ({items}) => {
+const OrderTab = ({ items }) => {
   const pagination = {
     clickable: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
   };
   return (
-    <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4">
-      
+    <div>
       <Swiper
         pagination={pagination}
         modules={[Pagination]}
         className="mySwiper"
       >
         <SwiperSlide>
-        {items.map((item) => (
-        <Card key={item._id} item={item}></Card>
-      ))}
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4">
+            {items.map((item) => (
+              <Card key={item._id} item={item}></Card>
+            ))}
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
