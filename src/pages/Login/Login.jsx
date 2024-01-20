cd09077b80ee2efd87602cae4e138e1e3bd06f50
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-01-20 20:14:00 +0600

    Create SignUp page and implement function

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
-
+import {AuthContext} from '../../providers/AuthProviders';
+import { Link } from 'react-router-dom';
 
 
 const Login = () => {
   const [disabled, setDisabled] = useState(true);
+
+  const {signIn} = useContext(AuthContext);
 
   useEffect(()=>{
       loadCaptchaEnginge(6); 
@@ -30,6 +33,11 @@ const Login = () => {
     const email = form.email.value;
     const password = form.password.value;
     // console.log(email, password);
+    signIn(email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
   };
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -91,6 +99,7 @@ const Login = () => {
               <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
             </div>
           </form>
+          <p><small>New Here? <Link to="/signup">Create an new account</Link></small></p>
         </div>
       </div>
     </div>
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProviders = ({ children }) => {
