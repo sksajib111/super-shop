922a4633fd2b28ac087e5ef16fe394d626ed86c4
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-02-12 23:49:34 +0600

    add to cart item query by email

--- src/hooks/useCarts.jsx
+++ src/hooks/useCarts.jsx
@@ -1,13 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./UseAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProviders";
 
 const useCarts = () => {
   // tan stack query
   const axiosSecure = useAxiosSecure();
+  const {user} = useContext(AuthContext);
+  console.log(user?.email);
   const { data: cart = [] } = useQuery({
-    queryKey: ['cart'],
+    queryKey: ['cart', user?.email],
     queryFn: async() => {
-      const res = await axiosSecure.get('/carts');
+      const res = await axiosSecure.get(`/carts?email=${user.email}`);
       return res.data;
     },
   });
