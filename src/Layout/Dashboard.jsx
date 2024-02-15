4eb2155b942416983dce36965391db9d10bcb79d
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-02-15 23:54:41 +0600

    Dashboard created

---
+++ src/Layout/Dashboard.jsx
@@ -0,0 +1,18 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <div className="flex">
+      <div className="w-64 min-h-full bg-orange-400">
+        <ul>
+          <li><NavLink to="/dashboard/cart"></NavLink></li>
+        </ul>
+      </div>
+      <div className="flex-1">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
