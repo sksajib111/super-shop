d42536720d6a496a065c892b1de936dd681fa16d
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-03-08 23:48:42 +0600

    Google logIn function implement

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,11 +1,26 @@
 import { FaGoogle } from "react-icons/fa";
+import { AuthContext } from "../../providers/AuthProviders";
+import { useContext } from "react";
 
 
 const SocialLogin = () => {
+  const { googleSignIn } = useContext(AuthContext);
+  // const {googleSignIn} = useAuth();
+
+  const handleGoogleSignIn = ()=>{
+    googleSignIn()
+    .then(result =>{
+      console.log(result.user);
+    })
+    .catch(error => {
+      console.log(error.massage);
+    })
+  };
+
   return (
     <div>
       <div>
-        <button className="btn">
+        <button onClick={handleGoogleSignIn} className="btn">
           <FaGoogle></FaGoogle>
           Google
         </button>
