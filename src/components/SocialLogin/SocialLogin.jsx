63b1f1f1825f765deed816c644b0f9473fdc326b
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-03-09 23:01:46 +0600

    social login system user authentication function creating

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,20 +1,32 @@
 import { FaGoogle } from "react-icons/fa";
 import { AuthContext } from "../../providers/AuthProviders";
 import { useContext } from "react";
-
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
   const { googleSignIn } = useContext(AuthContext);
   // const {googleSignIn} = useAuth();
+  const navigate = useNavigate();
 
-  const handleGoogleSignIn = ()=>{
+  const axiosPublic = useAxiosPublic();
+
+  const handleGoogleSignIn = () => {
     googleSignIn()
-    .then(result =>{
-      console.log(result.user);
-    })
-    .catch(error => {
-      console.log(error.massage);
-    })
+      .then((result) => {
+        console.log(result.user);
+        const userInfo = {
+          email: result.user?.email,
+          name: result.user?.displayName,
+        };
+        axiosPublic.post("/users", userInfo).then((res) => {
+          console.log(res.data);
+          navigate("/");
+        });
+      })
+      .catch((error) => {
+        console.log(error.massage);
+      });
   };
 
   return (
