50c2d59eaa5ed843545586f4792b6b200945fc30
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-03-16 23:40:59 +0600

    Dashboard admin role separated

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,43 +1,57 @@
-import {  FaBagShopping, FaCalendar, FaCartShopping, FaEnvelope, FaHouse, FaList, FaRankingStar, FaWallet } from "react-icons/fa6";
+import {
+  FaBagShopping,
+  FaEnvelope,
+  FaHouse,
+  FaListUl,
+  FaRegAddressBook,
+  FaUsers,
+  FaUtensils,
+} from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
-import useCarts from "../hooks/useCarts";
+// import useCarts from "../hooks/useCarts";
 
 const Dashboard = () => {
-  const [cart] = useCarts();
+  // const [cart] = useCarts();
+  
+  // TODO: get isAdmin value from the database
+  const isAdmin = true;
+
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-[#D1A054]">
         <ul className="menu">
-          <li>
-            <NavLink to="/dashboard/userHome">
-              <FaHouse></FaHouse> User Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/reservation">
-              <FaCalendar></FaCalendar> Reservation
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/paymentHistory">
-              <FaWallet></FaWallet> Payment History
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/cart">
-              <FaCartShopping></FaCartShopping> My Cart ({cart.length})
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/review">
-            <FaRankingStar /> Add Review
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/bookings">
-              <FaList></FaList> My Booking
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <></>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminHome">
+                  <FaHouse></FaHouse> Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/addItems">
+                <FaUtensils /> Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageItems">
+                <FaListUl /> Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageBookings">
+                <FaRegAddressBook /> Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/allUsers">
+                <FaUsers /> All Users
+                </NavLink>
+              </li>
+            </>
+          )}
+          {/* shared nav links */}
           <div className="divider divider-accent"></div>
           <li>
             <NavLink to="/">
@@ -46,12 +60,14 @@ const Dashboard = () => {
           </li>
           <li>
             <NavLink to="/order/salad">
-              <FaBagShopping />Menu
+              <FaBagShopping />
+              Menu
             </NavLink>
           </li>
           <li>
             <NavLink to="/order/contact">
-              <FaEnvelope />Contact
+              <FaEnvelope />
+              Contact
             </NavLink>
           </li>
         </ul>
